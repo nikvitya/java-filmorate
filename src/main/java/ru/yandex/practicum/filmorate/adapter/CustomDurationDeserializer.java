80c61af9fb6dbac1bb90647311887f3d31428e36
Vitaly Nikiforov <nikvitya@yandex.ru>
2023-04-11 17:23:13 +0300

    ТЗ 9. Привет! На проверку

--- src/main/java/ru/yandex/practicum/filmorate/adapter/CustomDurationDeserializer.java
+++ src/main/java/ru/yandex/practicum/filmorate/adapter/CustomDurationDeserializer.java
@@ -1,11 +1,8 @@
 package ru.yandex.practicum.filmorate.adapter;
 
-import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.deser.DurationDeserializer;
-import org.springframework.boot.jackson.JsonComponent;
 
 import java.io.IOException;
 import java.time.Duration;
