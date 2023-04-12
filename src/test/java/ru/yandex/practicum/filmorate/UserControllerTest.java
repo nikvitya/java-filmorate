7e135adeac5fe5d532eda4deb16360b89b799271
Vitaly Nikiforov <nikvitya@yandex.ru>
2023-04-12 23:27:13 +0300

    ТЗ 9. Исправил замечания

--- src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import org.springframework.http.HttpStatus;
-import ru.yandex.practicum.filmorate.controller.FilmController;
+
 import ru.yandex.practicum.filmorate.controller.UserController;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
