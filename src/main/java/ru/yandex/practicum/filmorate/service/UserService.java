dc95f3ab14b60b680b0738460f7b8c9e53ce4894
Vitaly Nikiforov <nikvitya@yandex.ru>
2023-05-20 17:08:38 +0300

    ТЗ 10. Привет. На проверку

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
+
 import ru.yandex.practicum.filmorate.model.User;
 
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
